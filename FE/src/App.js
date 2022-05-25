ec09be1dd6633607c5ac3945c7d4743b29692585
Adarsh khare <38292989+AdarshKhare29@users.noreply.github.com>
2022-05-25 15:33:20 +0530

    Update App.js

--- FE/src/App.js
+++ FE/src/App.js
@@ -3,12 +3,9 @@ import UserData from './components/UserData';
 import {useState} from 'react'
 import AlbumData from './components/AlbumData';
 function App() {
-  const[showAlbum,setShowAlbum]=useState(false)
-
   return (
     <>
     <UserData showAlbum={showAlbum} setShowAlbum={setShowAlbum}/>
-    {showAlbum && <AlbumData/>}
     </>
   );
 }
